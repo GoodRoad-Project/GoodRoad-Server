51c434e3f3da2961a5a818d6f800e62637f032db
KseniaG8 <gorodkovaksenia84@mail.ru>
2026-03-11 01:19:09 +0300

    feat: add route request/response models

---
+++ src/main/kotlin/com/GoodRoad/model/RouteRequest.kt
@@ -0,0 +1,37 @@
+package com.example.goodroad.features.network.models
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class RouteRequest(
+    val start: String,              // (lat,lon)
+    val end: String,                // (lat,lon)
+    @JsonProperty("user_id")
+    val userId: String,
+
+    @JsonProperty("max_stairs")
+    val maxStairsCount: Int? = null,        // сколько ступенек максимум
+    @JsonProperty("max_slope")
+    val maxSlopeAngle: Double? = null,       // макс угол уклона
+    @JsonProperty("max_curb_height")
+    val maxCurbHeight: Int? = null,          // макс высота бордюра
+    @JsonProperty("min_path_width")
+    val minPathWidth: Int? = null,            // мин ширина прохода
+
+    @JsonProperty("avoid_stairs")
+    val avoidStairs: Boolean = false,            // избегать лестниц
+    @JsonProperty("need_ramp")
+    val needRamp: Boolean = false,                // нужен пандус
+    @JsonProperty("avoid_bad_road")
+    val avoidBadRoad: Boolean = false,            // избегать плохих дорог
+
+    // какие поверхности избегать
+    @JsonProperty("avoid_surfaces")
+    val avoidSurfaceTypes: List<String> = emptyList(),  // "SAND", "GRAVEL"
+
+    // 👇 ДОПОЛНИТЕЛЬНЫЕ ПАРАМЕТРЫ
+    val locale: String = "ru",                    // язык инструкций
+    @JsonProperty("alternatives")
+    val needAlternatives: Boolean = true,          // нужны ли альтернативы
+    @JsonProperty("points_encoded")
+    val pointsEncoded: Boolean = true
+)
---
+++ src/main/kotlin/com/GoodRoad/model/gh/GraphHopperResponse.kt
@@ -0,0 +1,20 @@
+package com.goodroad.model.gh
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class GraphHopperResponse(
+    val path: List<Path>?,
+    val info: Info?
+)
+
+data class Path(
+    val distance: Double,
+    val time: Long,
+    val points: String,
+    @JsonProperty("points_encoded")
+    val pointsEncoded: Boolean = false
+)
+
+data class Info(
+    val took: Double
+)
