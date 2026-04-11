ec3128fa4ec30ea44dd5c2f02f7cebb1ae2e8411
SanriaArgos <argosi.sanria@gmail.com>
2026-04-12 00:11:04 +0300

    feat(user): add avatar upload support for profile photo

--- src/main/java/goodroad/users/users/UserController.java
+++ src/main/java/goodroad/users/users/UserController.java
@@ -1,12 +1,15 @@
 package goodroad.users.users;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
     private final UserSettingsService service;
 
     public UserController(UserSettingsService service) {
@@ -29,6 +32,17 @@ public class UserController {
     public void changePassword(@RequestParam String oldPassword, @RequestParam String newPassword) {
         String currentUsername = SecurityContextHolder.getContext().getAuthentication().getName();
         service.changePassword(currentUsername, oldPassword, newPassword);
+    }
+
+    @PostMapping(value = "/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public UserSettingsService.AvatarUploadResp uploadAvatar(@RequestParam("file") MultipartFile file) {
+        String currentUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+        return service.uploadAvatar(currentUsername, file);
+    }
+
+    @GetMapping("/avatar/{fileName}")
+    public ResponseEntity<Resource> getAvatar(@PathVariable String fileName) {
+        return service.getAvatar(fileName);
     }
 
     @DeleteMapping("")
