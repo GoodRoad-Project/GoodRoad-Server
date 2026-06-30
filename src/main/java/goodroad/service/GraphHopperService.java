5b21882a42884935c5f7d73fa23b49889787abdd
KseniaG8 <gorodkovaksenia84@mail.ru>
2026-06-30 14:34:09 +0300

    fix: fix GHService

--- src/main/java/goodroad/service/GraphHopperService.java
+++ src/main/java/goodroad/service/GraphHopperService.java
@@ -91,11 +91,6 @@ public class GraphHopperService {
             requestBody.put("profile", profile);
             requestBody.put("points_encoded", pointsEncoded);
 
-            requestBody.put("ch.disable", true);
-
-            requestBody.put("alternatives", true);
-            requestBody.put("alternative_route.max_paths", 3);
-
             if (customModel != null && !customModel.isEmpty()) {
                 requestBody.put("custom_model", customModel);
             }
